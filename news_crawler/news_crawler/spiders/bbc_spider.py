54c5643877ece7b4fb2eeb5754ed8667afff0866
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2024-06-28 15:50:40 -0300

    Add a new way to send documents to BigQuery

--- news_crawler/news_crawler/spiders/bbc_spider.py
+++ news_crawler/news_crawler/spiders/bbc_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import requests
 import os
+import pandas as pd
 
 from news_crawler.items import NewsArticle
 from readability import Document
@@ -20,10 +21,11 @@ class BbcSpider(scrapy.Spider):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        credentials = service_account.Credentials.from_service_account_file(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+        credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+        credentials = service_account.Credentials.from_service_account_file(credentials_path)
         self.client = bigquery.Client(credentials=credentials)
-        self.dataset_id = str(os.getenv('BIGQUERY_DATASET_ID'))
-        self.table_id = str(os.getenv('BIGQUERY_TABLE_ID')) 
+        self.dataset_id = os.getenv('BIGQUERY_DATASET_ID').split('.')[-1]
+        self.table_id = os.getenv('BIGQUERY_TABLE_ID').split('.')[-1]
 
     def parse(self, response):
         self.logger.info('Parsing main news page: %s', response.url)
@@ -50,20 +52,35 @@ class BbcSpider(scrapy.Spider):
         article['article_text'] = soup.get_text(separator="\n", strip=True)
 
         self.logger.info('Scraped article: %s', article['headline'])
+        self.send_to_bigquery(article)
         yield article
 
     def send_to_bigquery(self, article):
-        table_ref = self.client.dataset(self.dataset_id).table(self.table_id)
-        table = self.client.get_table(table_ref)
-        rows_to_insert = [{
+        data = [{
             'url': article['url'],
             'headline': article['headline'],
             'article_text': article['article_text'],
             'author': article['author'],
             'publication_date': article['publication_date']
         }]
-        errors = self.client.insert_rows_json(table, rows_to_insert)
-        if errors:
-            self.logger.error('Error inserting rows into BigQuery: %s', errors)
+
+        df = pd.DataFrame(data)
+
+        schema = [
+            bigquery.SchemaField("url", "STRING"),
+            bigquery.SchemaField("headline", "STRING"),
+            bigquery.SchemaField("article_text", "STRING"),
+            bigquery.SchemaField("author", "STRING"),
+            bigquery.SchemaField("publication_date", "STRING")
+        ]
+
+        table_ref = self.client.dataset(self.dataset_id).table(self.table_id)
+        job_config = bigquery.LoadJobConfig(schema=schema)
+        job = self.client.load_table_from_dataframe(df, table_ref, job_config=job_config)
+
+        job.result()
+
+        if job.errors:
+            self.logger.error('Error inserting rows into BigQuery: %s', job.errors)
         else:
             self.logger.info('Successfully inserted row into BigQuery')
